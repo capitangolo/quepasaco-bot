f16c9fe8b6637c3cf15b329fa22ee38de2696c77
Víctor Jiménez Cerrada <vjimenez@rootnode.es>
2023-06-22 00:39:39 +0200

    [feat] Read config from .env file if available

--- model/index.js
+++ model/index.js
@@ -3,6 +3,8 @@ module.exports = function () {
 
   // Init configuration
   console.log("Loading config…");
+  require('dotenv').config();
+
   m.c = {};
   m.c.DISCORD_BOT_TOKEN = process.env.DISCORD_BOT_TOKEN;
   m.c.DISCORD_CLIENT_ID = process.env.DISCORD_CLIENT_ID;
